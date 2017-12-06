e83333532119431fb993f129b1459fdb04dfe653
Jörn Zaefferer <joern.zaefferer@gmail.com>
2017-12-06 17:08:42 +0100

    remove blur leftovers

--- js/slide-deck.js
+++ js/slide-deck.js
@@ -286,32 +286,6 @@ SlideDeck.prototype.onBodyKeyDown_ = function(e) {
         media[media.paused ? "play" : "pause"]();
       }
       break;
-
-    case 49:
-    case 50:
-    case 51:
-    case 52:
-    case 53:
-      // don't mess with browser shortcuts for changing tabs
-      if (e.metaKey) {
-        return;
-      }
-      var strength = e.keyCode - 48;
-      var strengths = {
-        1: "blur-none",
-        2: "blur-weak",
-        3: "blur-medium",
-        4: "blur-strong",
-        5: "blur-black"
-      };
-      var classList = document.body.classList;
-      classList.remove("blur-none");
-      classList.remove("blur-weak");
-      classList.remove("blur-medium");
-      classList.remove("blur-strong");
-      classList.remove("blur-black");
-      classList.add( strengths[ strength ] );
-      break;
   }
 };
 
